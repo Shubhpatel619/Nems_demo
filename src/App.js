e31e8d534bf85f6b05acc0c642029be89a587489
inextrix <inextrix@inextrix.inextrix>
2026-02-26 14:45:41 +0530

    remove .env

--- src/App.js
+++ src/App.js
@@ -3,15 +3,11 @@ import NavBar from './components/NavBar';
 import Home from './components/Home';
 import About from './components/About';
 import Footer from './components/Footer';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    /* 
-      The basename property ensures the app works in the /Nems_demo/ subfolder on GitHub.
-      process.env.PUBLIC_URL is empty locally, so it won't break your local development.
-    */
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <NavBar title="NEMS" />
       <div className="content-wrapper">
         <Routes>
