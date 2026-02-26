d2e49d0e456ef27456061601c2f2220d1c1865aa
inextrix <inextrix@inextrix.inextrix>
2026-02-26 11:04:32 +0530

    GitHost.0

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import About from './components/About';
-import Footer from './components/Footer'; // Import the new Footer
+import Footer from './components/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+    /* 
+      The basename property ensures the app works in the /Nems_demo/ subfolder on GitHub.
+      process.env.PUBLIC_URL is empty locally, so it won't break your local development.
+    */
+    <Router basename={process.env.PUBLIC_URL}>
       <NavBar title="NEMS" />
       <div className="content-wrapper">
         <Routes>
@@ -15,7 +19,7 @@ function App() {
           <Route path="/about" element={<About />} />
         </Routes>
       </div>
-      <Footer /> {/* Add the Footer here */}
+      <Footer />
     </Router>
   );
 }
